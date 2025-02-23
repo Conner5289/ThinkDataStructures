f0ebcdcd238c9424555c2e7ef283ae99db8b0538
Conner Yule <conneryule@gmail.com>
2025-02-22 21:48:10 -0500

    delete

--- src/main/java/bad/robot/refactoring/chapter1/Rental.java
+++ src/main/java/bad/robot/refactoring/chapter1/Rental.java
@@ -42,4 +42,10 @@ public class Rental {
 
 	}
 
+	public int calculateFrequentRenterPoints() {
+		if (getMovie().getPriceCode() == Movie.NEW_RELEASE && getDaysRented() > 1)
+			return 2;
+		return 1;
+	}
+
 }
