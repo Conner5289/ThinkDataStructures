1a5153682b4d24b53b4ae24ee5dbf6bcbe93e5b5
Conner Yule <conneryule@gmail.com>
2025-02-22 20:33:13 -0500

    extract and move

--- src/main/java/bad/robot/refactoring/chapter1/Rental.java
+++ src/main/java/bad/robot/refactoring/chapter1/Rental.java
@@ -5,20 +5,41 @@ import java.util.List;
 
 public class Rental {
 
-    private Movie movie;
-    private int daysRented;
+	private Movie movie;
+	private int daysRented;
 
-    public Rental(Movie movie, int daysRented) {
-        this.movie = movie;
-        this.daysRented = daysRented;
-    }
+	public Rental(Movie movie, int daysRented) {
+		this.movie = movie;
+		this.daysRented = daysRented;
+	}
 
-    public Movie getMovie() {
-        return movie;
-    }
+	public Movie getMovie() {
+		return movie;
+	}
 
-    public int getDaysRented() {
-        return daysRented;
-    }
+	public int getDaysRented() {
+		return daysRented;
+	}
+
+	public double getChargeFor(Rental rental) {
+		double amount = 0;
+		switch (rental.getMovie().getPriceCode()) {
+			case Movie.REGULAR:
+				amount += 2;
+				if (rental.getDaysRented() > 2)
+					amount += (rental.getDaysRented() - 2) * 1.5;
+				break;
+			case Movie.NEW_RELEASE:
+				amount += rental.getDaysRented() * 3;
+				break;
+			case Movie.CHILDREN:
+				amount += 1.5;
+				if (rental.getDaysRented() > 3)
+					amount += (rental.getDaysRented() - 3) * 1.5;
+				break;
+		}
+		return amount;
+
+	}
 
 }
